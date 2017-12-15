c4cf6cc19b8f579234517e63dfcb888d09b0ad36
zhanghaitao1 <zhanghaitao1@longfor.com>
2017-12-15 12:06:12 +0800

    注册UI的添加

---
+++ longfor-ec/src/main/java/com/longfor/ec/sign/SignUpDelegate.java
@@ -0,0 +1,41 @@
+package com.longfor.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.view.View;
+
+import com.longfor.core.delegates.LongForDelegate;
+import com.longfor.ec.R;
+import com.longfor.ec.R2;
+
+import butterknife.BindView;
+
+/**
+ * Created by zhanghaitao1 on 2017/12/15.
+ */
+
+public class SignUpDelegate extends LongForDelegate {
+
+    @BindView(R2.id.edit_sign_up_name)
+    TextInputEditText mName = null;
+    @BindView(R2.id.edit_sign_up_email)
+    TextInputEditText mEmail = null;
+    @BindView(R2.id.edit_sign_up_phone)
+    TextInputEditText mPhone = null;
+    @BindView(R2.id.edit_sign_up_password)
+    TextInputEditText mPassword = null;
+    @BindView(R2.id.edit_sign_up_re_password)
+    TextInputEditText mRePassword = null;
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_sign_up;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+    }
+}
