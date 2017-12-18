ebeeb6edccce7963c26c90123a6e6cfdacda8741
zhanghaitao1 <zhanghaitao1@longfor.com>
2017-12-18 11:18:59 +0800

    添加用户的实体类

--- app/src/main/java/com/longfor/MainActivity.java
+++ app/src/main/java/com/longfor/MainActivity.java
@@ -1,34 +1,24 @@
 package com.longfor;
-
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.longfor.greendao.DaoMaster;
-import com.longfor.greendao.DaoSession;
-import com.longfor.greendao.User;
-import com.longfor.greendao.UserDao;
-
 public class MainActivity extends AppCompatActivity {
-
-
-    UserDao userDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "recluse-db", null);
-        SQLiteDatabase db = helper.getWritableDatabase();
-        DaoMaster daoMaster = new DaoMaster(db);
-        DaoSession daoSession = daoMaster.newSession();
-        userDao = daoSession.getUserDao();
-
-        User user = new User((long)1, "recluse");
-
-        userDao.insert(user);
-        userDao.update(user);
-        userDao.deleteByKey((long)1);
-        userDao.delete(user);
+//        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "recluse-db", null);
+//        SQLiteDatabase db = helper.getWritableDatabase();
+//        DaoMaster daoMaster = new DaoMaster(db);
+//        DaoSession daoSession = daoMaster.newSession();
+//        userDao = daoSession.getUserDao();
+//
+//        User user = new User((long)1, "recluse");
+//
+//        userDao.insert(user);
+//        userDao.update(user);
+//        userDao.deleteByKey((long)1);
+//        userDao.delete(user);
     }
 }
---
+++ longfor-ec/src/main/java/com/longfor/ec/sign/SignInDelegate.java
@@ -0,0 +1,81 @@
+package com.longfor.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
+import android.view.View;
+
+import com.longfor.core.delegates.LongForDelegate;
+import com.longfor.core.net.RestClient;
+import com.longfor.core.net.callback.ISuccess;
+import com.longfor.ec.R;
+import com.longfor.ec.R2;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Created by zhanghaitao1 on 2017/12/18.
+ */
+
+public class SignInDelegate extends LongForDelegate {
+
+    @BindView(R2.id.edit_sign_in_email)
+    TextInputEditText mEmail = null;
+    @BindView(R2.id.edit_sign_in_password)
+    TextInputEditText mPassword = null;
+
+
+    @OnClick(R2.id.btn_sign_in)
+    void onClickSignIn() {
+        if (checkForm()) {
+            RestClient.builder()
+                    .url("http://192.168.56.1:8080/RestDataServer/api/user_profile.php")
+                    .params("email", mEmail.getText().toString())
+                    .params("password", mPassword.getText().toString())
+                    .success(new ISuccess() {
+                        @Override
+                        public void onSuccess(String response) {
+
+                        }
+                    })
+                    .build()
+                    .post();
+        }
+    }
+
+    private boolean checkForm() {
+        final String email = mEmail.getText().toString();
+        final String password = mPassword.getText().toString();
+
+        boolean isPass = true;
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("错误的邮箱格式");
+            isPass = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("请填写至少6位数密码");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        return isPass;
+    }
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_sign_in;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+    }
+}
