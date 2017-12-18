1e7af20b2d4dee05fbe598da0194181fb4918eda
zhanghaitao1 <zhanghaitao1@longfor.com>
2017-12-18 10:03:47 +0800

    注册UI逻辑的添加

--- longfor-ec/src/main/java/com/longfor/ec/sign/SignUpDelegate.java
+++ longfor-ec/src/main/java/com/longfor/ec/sign/SignUpDelegate.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
 import android.view.View;
 
 import com.longfor.core.delegates.LongForDelegate;
+import com.longfor.core.net.RestClient;
+import com.longfor.core.net.callback.ISuccess;
 import com.longfor.ec.R;
 import com.longfor.ec.R2;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by zhanghaitao1 on 2017/12/15.
@@ -28,6 +32,73 @@ public class SignUpDelegate extends LongForDelegate {
     TextInputEditText mPassword = null;
     @BindView(R2.id.edit_sign_up_re_password)
     TextInputEditText mRePassword = null;
+
+    @OnClick(R2.id.btn_sign_up)
+    void onClickSignUp() {
+        if (checkForm()) {
+            RestClient.builder()
+                    .url("http://192.168.56.1:8080/RestDataServer/api/user_profile.php")
+                    .params("name", mName.getText().toString())
+                    .params("email", mEmail.getText().toString())
+                    .params("phone", mPhone.getText().toString())
+                    .params("password", mPassword.getText().toString())
+                    .success(new ISuccess() {
+                        @Override
+                        public void onSuccess(String response) {
+
+                        }
+                    })
+                    .build()
+                    .post();
+        }
+    }
+
+    private boolean checkForm() {
+        final String name = mName.getText().toString();
+        final String email = mEmail.getText().toString();
+        final String phone = mPhone.getText().toString();
+        final String password = mPassword.getText().toString();
+        final String rePassword = mRePassword.getText().toString();
+
+        boolean isPass = true;
+
+        if (name.isEmpty()) {
+            mName.setError("请输入姓名");
+            isPass = false;
+        } else {
+            mName.setError(null);
+        }
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("错误的邮箱格式");
+            isPass = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+        if (phone.isEmpty() || phone.length() != 11) {
+            mPhone.setError("手机号码错误");
+            isPass = false;
+        } else {
+            mPhone.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("请填写至少6位数密码");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        if (rePassword.isEmpty() || rePassword.length() < 6 || !(rePassword.equals(password))) {
+            mRePassword.setError("密码验证错误");
+            isPass = false;
+        } else {
+            mRePassword.setError(null);
+        }
+
+        return isPass;
+    }
 
     @Override
     public Object setLayout() {
