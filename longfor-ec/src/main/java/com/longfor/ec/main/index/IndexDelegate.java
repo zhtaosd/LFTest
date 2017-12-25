3635226d8661a099863e32d51f57a382a7aa8827
zhanghaitao1 <zhanghaitao1@longfor.com>
2017-12-25 20:07:42 +0800

    索引布局的添加

--- longfor-ec/src/main/java/com/longfor/ec/main/index/IndexDelegate.java
+++ longfor-ec/src/main/java/com/longfor/ec/main/index/IndexDelegate.java
@@ -3,16 +3,35 @@ package com.longfor.ec.main.index;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.joanzapata.iconify.widget.IconTextView;
 import com.longfor.core.delegates.bottom.BottomItemDelegate;
 import com.longfor.ec.R;
+import com.longfor.ec.R2;
+
+import butterknife.BindView;
 
 /**
  * Created by zhanghaitao1 on 2017/12/25.
  */
 
 public class IndexDelegate extends BottomItemDelegate {
+
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView = null;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout = null;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar = null;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan = null;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView = null;
 
     @Override
     public Object setLayout() {
@@ -22,5 +41,18 @@ public class IndexDelegate extends BottomItemDelegate {
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
 
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        initRefreshLayout();
+    }
+
+    private void initRefreshLayout() {
+        mRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        mRefreshLayout.setProgressViewOffset(true,120,300);
     }
 }
